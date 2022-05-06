1b0f4230d18971e3b0564b50d72912b2fb90deb0
AlexHuescaBenito <alex.huesca.7e5@itb.cat>
2022-05-06 10:24:14 +0200

    si

--- JS/AdivinaCodigoSecreto-Enunciado/script.js
+++ JS/AdivinaCodigoSecreto-Enunciado/script.js
@@ -1,28 +1,56 @@
 const codigo = [];
-const maxIntento = 8;
+const maxIntento = 7;
+let intentos = 0;
+
+function actualizaIntentos(){
+    if (intentos < maxIntento){
+        document.getElementById("info").innerText = "Te quedan " + (maxIntento - intentos + " intentos");
+    }
+    else {
+        document.getElementById("info").innerText = "Te queda " + maxIntento - intentos + " intento";
+        if (intentos == maxIntento){
+            document.getElementById("info").innerText = "Fin del juego. Has perdido bobo."
+            document.getElementById("check").disabled = true;
+        }
+    }
+    intentos++;
+    
+}
 
 /*1. Genera una constante CODIGO_SECRETO de tipo array de 5 número aleatorios entre 0 y 9 usando la libreria Math.random();*/
 function codigoSecreto() {
     for (let i = 0; i < 5; i++) {
-        codigo[i] = Math.floor((Math.random() * 10));
+        codigo[i] = Math.floor(Math.random() * 10);
     }
 }
-
-codigoSecreto();
 
 /*
 2.Comprobar si el número introducido es correcto
 */
 function Comprobar(){
-    let codigoUser = document.getElementById("codigo");
-    let row = document.getElementsByClassName("rowResult w100 flex wrap")
-    let cell = row[1].getElementsByClassName("w20")
-    for (let i = 0; i < row.length; i++){
-        for (i = 0; i < cell.length; i++){
-            if (cell[i]==codigoUser[i]){
-                cell[i].style.color = "green";
-            } 
-        
+    
+    let codigoUser = document.getElementById("numero").value;
+    let rows = document.getElementsByClassName("rowResult")
+    let cells = rows[intentos].getElementsByClassName("celResult")
+    for (i=0; i<codigo.length; i++){
+        cells[i].innerText = codigoUser[i];
+        if (codigoUser[i] == codigo[i]){
+            cells[i].style.backgroundColor = "green";
+        }
+        else{
+            for (j=0; j<5;j++){
+                if(codigoUser[i]==codigo[j]){
+                    cells[i].style.backgroundColor = "yellow";
+                    break;
+                }
+                else{
+                    cells[i].style.backgroundColor = "grey";
+                }
+            }
         }
     }
+    actualizaIntentos()
 }
+codigoSecreto()
+
+console.log(codigo)
